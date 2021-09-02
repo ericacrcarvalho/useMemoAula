fc7435069b25029680fbc8077d1337dd8b395b1c
Erica Carvalho <ericacrcarvalho@gmail.com>
2021-09-01 22:26:56 -0300

    completo

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useMemo, useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export default function App() {
-  return (
+
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const somar = () => {
+    console.log("Somei");
+    return parseInt(x) + parseInt(y);
+  }
+
+  const resultado = useMemo(() => {
+    return somar();
+  }, [x,y]);
+
+  useEffect(() => {
+    console.log("useEffect");
+  }, [total]);
+
+  return ( 
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={[styles.text, {fontWeight:'bold'}]}>useMemo</Text>
+      <Text style={styles.text}>CALCULADORA</Text>
+      <TextInput
+            keyboardType="numeric"
+            placeholder={0}
+            onChangeText={(valor) => setX(valor.length > 0 ? valor : 0)}
+            style={styles.textInput}
+        />
+         <TextInput
+            keyboardType="numeric"
+            placeholder={0}
+            onChangeText={(valor) => setY(valor.length > 0 ? valor : 0)}
+            style={styles.textInput}
+        />
+        <Button
+          mode="contained"
+          onPress={() => setTotal(resultado)}
+        >somar</Button>
+        <Text style={styles.text}>Resultado: {total}</Text>
     </View>
   );
 }
@@ -18,4 +53,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    borderWidth: 2,
+    padding: 4,
+    margin: 2,
+    width: 100,
+    textAlign: 'center',
+    margin: 6,
+  },
+  text: {
+    margin: 6,
+    fontSize: 18,
+  }
 });
